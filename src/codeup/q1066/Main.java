737475c3fd138424b062bd49b28a91c39726169f
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-06-21 00:52:03 +0900

    Create Main.java

---
+++ src/codeup/q1066/Main.java
@@ -0,0 +1,29 @@
+package codeup.q1066;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		
+		int[] arr = {n1, n2, n3};
+		/*
+		for(int i=0; i<arr.length; i++) {
+			String result = arr[i] % 2 == 0 ? "even" : "odd";
+			System.out.println(result);
+		}
+		*/
+		for(int n : arr) {
+			System.out.println(n % 2 == 0 ? "even": "odd");
+		}
+		
+		sc.close();
+	}
+
+}
