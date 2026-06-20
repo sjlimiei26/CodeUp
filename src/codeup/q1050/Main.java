2e70c9aa0ad72bb8d747aa736730498c96e3a8c1
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-06-21 00:44:42 +0900

    Create Main.java

---
+++ src/codeup/q1050/Main.java
@@ -0,0 +1,19 @@
+package codeup.q1050;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		int result = n1 == n2 ? 1 : 0;
+		System.out.println(result);
+		
+		sc.close();
+	}
+
+}
