b33105bd9248b892f7fdb65cb8dce2b5525848fb
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-06-20 23:37:37 +0900

    Create Main.java

---
+++ src/codeup/q1014/Main.java
@@ -0,0 +1,18 @@
+package codeup.q1014;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String ch1 = sc.next();
+		String ch2 = sc.next();
+		
+		System.out.printf("%s %s", ch2, ch1);
+		
+		sc.close();
+	}
+
+}
