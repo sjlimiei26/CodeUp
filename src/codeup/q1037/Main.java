9049d6b456bfb9c085f2bfa74d6122b7f42ef4f9
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-06-20 23:46:42 +0900

    Create Main.java

---
+++ src/codeup/q1037/Main.java
@@ -0,0 +1,17 @@
+package codeup.q1037;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.printf("%c", num);
+
+		sc.close();
+	}
+
+}
