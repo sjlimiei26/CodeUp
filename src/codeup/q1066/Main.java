670d8629bfdb70c10a34ae5c2d4826ccdec7dc5d
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-06-21 00:53:05 +0900

    Update Main.java

--- src/codeup/q1066/Main.java
+++ src/codeup/q1066/Main.java
@@ -8,11 +8,19 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 
+		/*
 		int n1 = sc.nextInt();
 		int n2 = sc.nextInt();
 		int n3 = sc.nextInt();
 		
 		int[] arr = {n1, n2, n3};
+		*/
+		
+		int[] arr = new int[3];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
 		/*
 		for(int i=0; i<arr.length; i++) {
 			String result = arr[i] % 2 == 0 ? "even" : "odd";
